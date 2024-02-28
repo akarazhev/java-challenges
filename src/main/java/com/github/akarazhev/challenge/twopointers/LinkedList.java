46b9ee3c5efb075647131a0beca187cd631f78db
Andrey Karazhev <andrey.karazhev@specific-group.com>
2024-02-28 10:01:20 +0300

    Refactored the solution.

--- src/main/java/com/github/akarazhev/challenge/twopointers/LinkedList.java
+++ src/main/java/com/github/akarazhev/challenge/twopointers/LinkedList.java
@@ -12,19 +12,17 @@ public class LinkedList {
     // insertNodeAtHead method will insert a LinkedListNode at head
     // of a linked list.
     public void insertNodeAtHead(LinkedListNode node) {
-        if (this.head == null) {
-            this.head = node;
-        } else {
+        if (this.head != null) {
             node.next = this.head;
-            this.head = node;
         }
+
+        this.head = node;
     }
     // createLinkedList method will create the linked list using the
     // given integer array with the help of InsertAtHead method.
-    public void createLinkedList(int[] lst) {
-        for (int i = lst.length - 1; i >= 0; i--) {
-            LinkedListNode newNode = new LinkedListNode(lst[i]);
-            insertNodeAtHead(newNode);
+    public void createLinkedList(int[] list) {
+        for (int i = list.length - 1; i >= 0; i--) {
+            insertNodeAtHead(new LinkedListNode(list[i]));
         }
     }
 }
