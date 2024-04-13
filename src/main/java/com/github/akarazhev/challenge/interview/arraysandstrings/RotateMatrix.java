91b30cfe336234586b244e77a3762c4011744dda
Andrey Karazhev <karazhev@gmail.com>
2024-04-14 00:23:09 +0300

    Updated the "Rotate Matrix" task and tests.

--- src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/RotateMatrix.java
+++ src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/RotateMatrix.java
@@ -1,4 +1,38 @@
 package com.github.akarazhev.challenge.interview.arraysandstrings;
 
+/**
+ * Provides <code>Rotate Matrix</code> solutions.
+ */
 public class RotateMatrix {
+
+    /**
+     * This function takes a matrix and writes a method to rotate the image by 90 degrees.
+     *
+     * @param m the matrix
+     * @return the result of operation
+     */
+    public static boolean rotate(int[][] m) {
+        if (m.length == 0 || m.length != m[0].length) {
+            return false; // Not a square
+        }
+
+        int n = m.length;
+        for (int layer = 0; layer < n / 2; layer++) {
+            int last = n - 1 - layer;
+            for (int i = layer; i < last; i++) {
+                int offset = i - layer;
+                int top = m[layer][i]; // save top
+                // left -> top
+                m[layer][i] = m[last - offset][layer];
+                // bottom -> left
+                m[last - offset][layer] = m[last][last - offset];
+                // right -> bottom
+                m[last][last - offset] = m[i][last];
+                // top -> right
+                m[i][last] = top; // right <- saved top
+            }
+        }
+
+        return true;
+    }
 }
--- src/test/java/com/github/akarazhev/challenge/interview/arraysandstrings/RotateMatrixTest.java
+++ src/test/java/com/github/akarazhev/challenge/interview/arraysandstrings/RotateMatrixTest.java
@@ -1,4 +1,19 @@
 package com.github.akarazhev.challenge.interview.arraysandstrings;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Provides tests for the <code>Rotate Matrix</code> solutions.
+ */
+@DisplayName("Rotate Matrix Test")
 public class RotateMatrixTest {
+
+    @Test
+    @DisplayName("Test Rotate")
+    void testRotate() {
+        assertTrue(RotateMatrix.rotate(new int[][]{{3, 3}, {0, 9}}));
+    }
 }
