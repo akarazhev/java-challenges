e3d661b570328f465ed80018758455fd560b197c
Andrey Karazhev <karazhev@gmail.com>
2024-04-14 00:34:22 +0300

    Updated the "String Rotation" task and tests.

--- src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/StringRotation.java
+++ src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/StringRotation.java
@@ -1,4 +1,26 @@
 package com.github.akarazhev.challenge.interview.arraysandstrings;
 
+/**
+ * Provides <code>String Rotation</code> solutions.
+ */
 public class StringRotation {
+
+    /**
+     * This function takes two strings and checks if one word is a substring of another.
+     *
+     * @param s1 first string
+     * @param s2 second string
+     * @return the result of operation
+     */
+    public static boolean isRotation(String s1, String s2) {
+        int len = s1.length();
+        /* check that s1 and s2 are equal length and not empty */
+        if (len == s2.length() && len > 0) {
+            /* concatenate s1 and s1 within new buffer */
+            String s1s1 = s1 + s1;
+            return s1s1.contains(s2);
+        }
+
+        return false;
+    }
 }
--- src/test/java/com/github/akarazhev/challenge/interview/arraysandstrings/StringRotationTest.java
+++ src/test/java/com/github/akarazhev/challenge/interview/arraysandstrings/StringRotationTest.java
@@ -1,4 +1,22 @@
 package com.github.akarazhev.challenge.interview.arraysandstrings;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Provides tests for the <code>String Rotation</code> solutions.
+ */
+@DisplayName("String Rotation Test")
 public class StringRotationTest {
+
+    @Test
+    @DisplayName("Test Is Rotation")
+    void testIsRotation() {
+        assertTrue(StringRotation.isRotation("apple", "pleap"));
+        assertTrue(StringRotation.isRotation("waterbottle", "erbottlewat"));
+        assertFalse(StringRotation.isRotation("camera", "macera"));
+    }
 }
