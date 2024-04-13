6229838bae38c5adfd8b55fc012e77c004b4fa75
Andrey Karazhev <karazhev@gmail.com>
2024-04-13 23:26:44 +0300

    Updated the "One Away" task and tests.

--- src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/OneAway.java
+++ src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/OneAway.java
@@ -1,4 +1,114 @@
 package com.github.akarazhev.challenge.interview.arraysandstrings;
 
+/**
+ * Provides <code>One Away</code> solutions.
+ */
 public class OneAway {
+
+    /**
+     * First solution
+     */
+    static class FirstSolution {
+
+        private static boolean oneEditReplace(String s1, String s2) {
+            boolean foundDifference = false;
+            for (int i = 0; i < s1.length(); i++) {
+                if (s1.charAt(i) != s2.charAt(i)) {
+                    if (foundDifference) {
+                        return false;
+                    }
+
+                    foundDifference = true;
+                }
+            }
+
+            return true;
+        }
+
+        /* Check if you can insert a character into s1 to make s2. */
+        private static boolean oneEditInsert(String s1, String s2) {
+            int index1 = 0;
+            int index2 = 0;
+            while (index2 < s2.length() && index1 < s1.length()) {
+                if (s1.charAt(index1) != s2.charAt(index2)) {
+                    if (index1 != index2) {
+                        return false;
+                    }
+
+                    index2++;
+                } else {
+                    index1++;
+                    index2++;
+                }
+            }
+
+            return true;
+        }
+
+        /**
+         * This function takes two strings and checks if they are one edit (or zero edits) away.
+         *
+         * @param first the first string
+         * @param second the second string
+         * @return the result of operation
+         */
+        public static boolean oneEditAway(String first, String second) {
+            if (first.length() == second.length()) {
+                return oneEditReplace(first, second);
+            } else if (first.length() + 1 == second.length()) {
+                return oneEditInsert(first, second);
+            } else if (first.length() - 1 == second.length()) {
+                return oneEditInsert(second, first);
+            }
+
+            return false;
+        }
+    }
+
+    /**
+     * Second solution
+     */
+    static class SecondSolution {
+
+        /**
+         * This function takes two strings and checks if they are one edit (or zero edits) away.
+         *
+         * @param first  the first string
+         * @param second the second string
+         * @return the result of operation
+         */
+        public static boolean oneEditAway(String first, String second) {
+            /* Length checks. */
+            if (Math.abs(first.length() - second.length()) > 1) {
+                return false;
+            }
+
+            /* Get shorter and longer string.*/
+            String s1 = first.length() < second.length() ? first : second;
+            String s2 = first.length() < second.length() ? second : first;
+
+            int index1 = 0;
+            int index2 = 0;
+            boolean foundDifference = false;
+            while (index2 < s2.length() && index1 < s1.length()) {
+                if (s1.charAt(index1) != s2.charAt(index2)) {
+                    /* Ensure that this is the first difference found.*/
+                    if (foundDifference) {
+                        return false;
+                    }
+
+                    foundDifference = true;
+                    if (s1.length() == s2.length()) { // On replace, move shorter pointer
+                        index1++;
+                    }
+                } else {
+                    index1++; // If matching, move shorter pointer
+                }
+
+                index2++; // Always move pointer for longer string
+            }
+
+            return true;
+        }
+    }
 }
--- src/test/java/com/github/akarazhev/challenge/interview/arraysandstrings/OneAwayTest.java
+++ src/test/java/com/github/akarazhev/challenge/interview/arraysandstrings/OneAwayTest.java
@@ -1,4 +1,50 @@
 package com.github.akarazhev.challenge.interview.arraysandstrings;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Provides tests for the <code>One Away</code> solutions.
+ */
+@DisplayName("One Away  Test")
 public class OneAwayTest {
+
+    @Test
+    @DisplayName("Test First Solution One Edit Away")
+    void testFirstSolutionOneEditAway() {
+        assertTrue(OneAway.FirstSolution.oneEditAway("a", "b"));
+        assertTrue(OneAway.FirstSolution.oneEditAway("", "d"));
+        assertTrue(OneAway.FirstSolution.oneEditAway("d", "de"));
+        assertFalse(OneAway.FirstSolution.oneEditAway("pale", "pse"));
+        assertTrue(OneAway.FirstSolution.oneEditAway("acdsfdsfadsf", "acdsgdsfadsf"));
+        assertTrue(OneAway.FirstSolution.oneEditAway("acdsfdsfadsf", "acdsfdfadsf"));
+        assertTrue(OneAway.FirstSolution.oneEditAway("acdsfdsfadsf", "acdsfdsfads"));
+        assertTrue(OneAway.FirstSolution.oneEditAway("acdsfdsfadsf", "cdsfdsfadsf"));
+        assertFalse(OneAway.FirstSolution.oneEditAway("adfdsfadsf", "acdfdsfdsf"));
+        assertFalse(OneAway.FirstSolution.oneEditAway("adfdsfadsf", "bdfdsfadsg"));
+        assertFalse(OneAway.FirstSolution.oneEditAway("adfdsfadsf", "affdsfads"));
+        assertTrue(OneAway.FirstSolution.oneEditAway("pale", "pkle"));
+        assertFalse(OneAway.FirstSolution.oneEditAway("pkle", "pable"));
+    }
+
+    @Test
+    @DisplayName("Test Second Solution One Edit Away")
+    void testSecondSolutionOneEditAway() {
+        assertTrue(OneAway.SecondSolution.oneEditAway("a", "b"));
+        assertTrue(OneAway.SecondSolution.oneEditAway("", "d"));
+        assertTrue(OneAway.SecondSolution.oneEditAway("d", "de"));
+        assertFalse(OneAway.SecondSolution.oneEditAway("pale", "pse"));
+        assertTrue(OneAway.SecondSolution.oneEditAway("acdsfdsfadsf", "acdsgdsfadsf"));
+        assertTrue(OneAway.SecondSolution.oneEditAway("acdsfdsfadsf", "acdsfdfadsf"));
+        assertTrue(OneAway.SecondSolution.oneEditAway("acdsfdsfadsf", "acdsfdsfads"));
+        assertTrue(OneAway.SecondSolution.oneEditAway("acdsfdsfadsf", "cdsfdsfadsf"));
+        assertFalse(OneAway.SecondSolution.oneEditAway("adfdsfadsf", "acdfdsfdsf"));
+        assertFalse(OneAway.SecondSolution.oneEditAway("adfdsfadsf", "bdfdsfadsg"));
+        assertFalse(OneAway.SecondSolution.oneEditAway("adfdsfadsf", "affdsfads"));
+        assertTrue(OneAway.SecondSolution.oneEditAway("pale", "pkle"));
+        assertFalse(OneAway.SecondSolution.oneEditAway("pkle", "pable"));
+    }
 }
