0e0b5819b772df4c735e9949604320ad66e374f0
Andrey Karazhev <karazhev@gmail.com>
2024-04-23 20:57:15 +0300

    Refactored the code.

--- src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/CheckPermutation.java
+++ src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/CheckPermutation.java
@@ -1,58 +1,44 @@
 package com.github.akarazhev.challenge.interview.arraysandstrings;
 
 /**
- * Provides <code>Check Permutation</code> solutions.
+ * Statement: Given two strings, write a method to decide if one is a permutation of the other.
  */
 public class CheckPermutation {
 
     /**
-     * First Solution
+     * First Solution: The function takes two strings and evaluates if one is a permutation of the other.
      */
     static class FirstSolution {
 
-        private static String sort(String s) {
-            char[] content = s.toCharArray();
+        private static String sort(String string) {
+            char[] content = string.toCharArray();
             java.util.Arrays.sort(content);
             return new String(content);
         }
 
-        /**
-         * This function takes two strings and evaluates if one is a permutation of the other.
-         *
-         * @param s the first string
-         * @param t the second string
-         * @return the result of operation
-         */
-        public static boolean permutation(String s, String t) {
-            return sort(s).equals(sort(t));
+        public static boolean permutation(String firstString, String secondString) {
+            return sort(firstString).equals(sort(secondString));
         }
     }
 
     /**
-     * Second Solution
+     * Second Solution: The function takes two strings and evaluates if one is a permutation of the other.
      */
     static class SecondSolution {
 
-        /**
-         * This function takes two strings and evaluates if one is a permutation of the other.
-         *
-         * @param s the first string
-         * @param t the second string
-         * @return the result of operation
-         */
-        public static boolean permutation(String s, String t) {
-            if (s.length() != t.length()) {
+        public static boolean permutation(String firstString, String secondString) {
+            if (firstString.length() != secondString.length()) {
                 return false; // Permutations must be same length
             }
 
             int[] letters = new int[128]; // Assumption: ASCII
-            for (int i = 0; i < s.length(); i++) {
-                letters[s.charAt(i)]++;
+            for (int i = 0; i < firstString.length(); i++) {
+                letters[firstString.charAt(i)]++;
             }
 
-            for (int i = 0; i < t.length(); i++) {
-                letters[t.charAt(i)]--;
-                if (letters[t.charAt(i)] < 0) {
+            for (int i = 0; i < secondString.length(); i++) {
+                letters[secondString.charAt(i)]--;
+                if (letters[secondString.charAt(i)] < 0) {
                     return false;
                 }
             }
--- src/test/java/com/github/akarazhev/challenge/interview/arraysandstrings/CheckPermutationTest.java
+++ src/test/java/com/github/akarazhev/challenge/interview/arraysandstrings/CheckPermutationTest.java
@@ -7,22 +7,23 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- * Provides tests for the <code>Check Permutation</code> solutions.
+ * Provides tests for the statement:
+ * Given two strings, write a method to decide if one is a permutation of the other.
  */
-@DisplayName("Check Permutation Test")
+@DisplayName("Check String Permutation Test")
 public class CheckPermutationTest {
 
     @Test
-    @DisplayName("Test First Solution Permutation")
-    void testFirstSolutionPermutation() {
+    @DisplayName("Test First Solution")
+    void testFirstSolution() {
         assertTrue(CheckPermutation.FirstSolution.permutation("apple", "papel"));
         assertTrue(CheckPermutation.FirstSolution.permutation("carrot", "tarroc"));
         assertFalse(CheckPermutation.FirstSolution.permutation("hello", "llloh"));
     }
 
     @Test
-    @DisplayName("Test Second Solution Permutation")
-    void testSecondSolutionPermutation() {
+    @DisplayName("Test Second Solution")
+    void testSecondSolution() {
         assertTrue(CheckPermutation.SecondSolution.permutation("apple", "papel"));
         assertTrue(CheckPermutation.SecondSolution.permutation("carrot", "tarroc"));
         assertFalse(CheckPermutation.SecondSolution.permutation("hello", "llloh"));
