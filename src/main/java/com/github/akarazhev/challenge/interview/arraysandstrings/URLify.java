f6bd1c728b5a7660a50ec617473009954b21692e
Andrey Karazhev <karazhev@gmail.com>
2024-04-23 21:33:09 +0300

    Refactored the code.

--- src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/URLify.java
+++ src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/URLify.java
@@ -1,8 +1,8 @@
 package com.github.akarazhev.challenge.interview.arraysandstrings;
 
 /**
- * Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at
- * the end to hold the additional characters,and that you are given the "true" length of the string.
+ * Statement: Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient
+ * space at the end to hold the additional characters,and that you are given the "true" length of the string.
  * (Note: Please use a character array so that you can perform this operation in place.)
  * <p>
  * EXAMPLE
