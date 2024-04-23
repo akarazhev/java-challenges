550a5e81276961462b2253f055fc7680a8742217
Andrey Karazhev <karazhev@gmail.com>
2024-04-23 22:18:07 +0300

    Refactored and commented the code.

--- src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/RotateMatrix.java
+++ src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/RotateMatrix.java
@@ -1,35 +1,33 @@
 package com.github.akarazhev.challenge.interview.arraysandstrings;
 
 /**
- * Provides <code>Rotate Matrix</code> solutions.
+ * Statement: Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method
+ * to rotate the image by 90 degrees. Can you do this in place?
  */
 public class RotateMatrix {
 
     /**
      * This function takes a matrix and writes a method to rotate the image by 90 degrees.
-     *
-     * @param m the matrix
-     * @return the result of operation
      */
-    public static boolean rotate(int[][] m) {
-        if (m.length == 0 || m.length != m[0].length) {
+    public static boolean rotate(int[][] matrix) {
+        if (matrix.length == 0 || matrix.length != matrix[0].length) {
             return false; // Not a square
         }
 
-        int n = m.length;
+        int n = matrix.length;
         for (int layer = 0; layer < n / 2; layer++) {
             int last = n - 1 - layer;
             for (int i = layer; i < last; i++) {
                 int offset = i - layer;
-                int top = m[layer][i]; // save top
+                int top = matrix[layer][i]; // save top
                 // left -> top
-                m[layer][i] = m[last - offset][layer];
+                matrix[layer][i] = matrix[last - offset][layer];
                 // bottom -> left
-                m[last - offset][layer] = m[last][last - offset];
+                matrix[last - offset][layer] = matrix[last][last - offset];
                 // right -> bottom
-                m[last][last - offset] = m[i][last];
+                matrix[last][last - offset] = matrix[i][last];
                 // top -> right
-                m[i][last] = top; // right <- saved top
+                matrix[i][last] = top; // right <- saved top
             }
         }
 
