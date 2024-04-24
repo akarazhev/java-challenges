7e292629ab4a881903af3f58bf519be8eceee5fb
Andrey Karazhev <karazhev@gmail.com>
2024-04-24 22:42:35 +0300

    Refactored and commented the code.

--- src/test/java/com/github/akarazhev/challenge/interview/arraysandstrings/RotateMatrixTest.java
+++ src/test/java/com/github/akarazhev/challenge/interview/arraysandstrings/RotateMatrixTest.java
@@ -3,10 +3,12 @@ package com.github.akarazhev.challenge.interview.arraysandstrings;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- * Provides tests for the <code>Rotate Matrix</code> solutions.
+ * Provides tests for the statement: Given an image represented by an NxN matrix, where each pixel in the image is
+ * 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
  */
 @DisplayName("Rotate Matrix Test")
 public class RotateMatrixTest {
@@ -14,6 +16,11 @@ public class RotateMatrixTest {
     @Test
     @DisplayName("Test Rotate")
     void testRotate() {
-        assertTrue(RotateMatrix.rotate(new int[][]{{3, 3}, {0, 9}}));
+        int[][] matrix = new int[][]{{3, 3}, {0, 9}};
+        assertTrue(RotateMatrix.rotate(matrix));
+        assertEquals(0, matrix[0][0]);
+        assertEquals(3, matrix[0][1]);
+        assertEquals(3, matrix[1][1]);
+        assertEquals(9, matrix[1][0]);
     }
 }
