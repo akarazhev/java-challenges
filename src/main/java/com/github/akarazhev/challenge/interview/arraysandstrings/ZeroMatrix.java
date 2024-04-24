0679a98806f80364a4da7cb19ee06a0e3319e2ee
Andrey Karazhev <karazhev@gmail.com>
2024-04-24 22:56:56 +0300

    Refactored and commented the code.

--- src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/ZeroMatrix.java
+++ src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/ZeroMatrix.java
@@ -1,61 +1,32 @@
 package com.github.akarazhev.challenge.interview.arraysandstrings;
 
 /**
- * Provides <code>Zero Matrix</code> solution.
+ * Statement: Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0.
  */
 public class ZeroMatrix {
 
-    /**
-     * First solution
-     */
-    static class FirstSolution {
+    private static class Common {
 
-        private static void nullifyRow(int[][] m, int r) {
-            for (int j = 0; j < m[0].length; j++) {
-                m[r][j] = 0;
+        private static void nullifyRow(int[][] matrix, int row) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[row][j] = 0;
             }
         }
 
-        private static void nullifyColumn(int[][] m, int c) {
-            for (int i = 0; i < m.length; i++) {
-                m[i][c] = 0;
+        private static void nullifyColumn(int[][] matrix, int col) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][col] = 0;
             }
         }
 
-        public static void setZeros(int[][] m) {
-            boolean[] row = new boolean[m.length];
-            boolean[] column = new boolean[m[0].length];
-            // Store the row and column index with value 0
-            for (int i = 0; i < m.length; i++) {
-                for (int j = 0; j < m[0].length; j++) {
-                    if (m[i][j] == 0) {
-                        row[i] = true;
-                        column[j] = true;
-                    }
-                }
-            }
-            // Nullify rows
-            for (int i = 0; i < row.length; i++) {
-                if (row[i]) {
-                    nullifyRow(m, i);
-                }
-            }
-            // Nullify columns
-            for (int j = 0; j < column.length; j++) {
-                if (column[j]) {
-                    nullifyColumn(m, j);
-                }
-            }
-        }
-
-        private static boolean matricesAreEqual(int[][] m1, int[][] m2) {
-            if (m1.length != m2.length || m1[0].length != m2[0].length) {
+        private static boolean matricesAreEqual(int[][] firstMatrix, int[][] secondMatrix) {
+            if (firstMatrix.length != secondMatrix.length || firstMatrix[0].length != secondMatrix[0].length) {
                 return false;
             }
 
-            for (int k = 0; k < m1.length; k++) {
-                for (int j = 0; j < m1[0].length; j++) {
-                    if (m1[k][j] != m2[k][j]) {
+            for (int k = 0; k < firstMatrix.length; k++) {
+                for (int j = 0; j < firstMatrix[0].length; j++) {
+                    if (firstMatrix[k][j] != secondMatrix[k][j]) {
                         return false;
                     }
                 }
@@ -64,11 +35,11 @@ public class ZeroMatrix {
             return true;
         }
 
-        private static int[][] cloneMatrix(int[][] m) {
-            int[][] c = new int[m.length][m[0].length];
-            for (int i = 0; i < m.length; i++) {
-                for (int j = 0; j < m[0].length; j++) {
-                    c[i][j] = m[i][j];
+        private static int[][] cloneMatrix(int[][] matrix) {
+            int[][] c = new int[matrix.length][matrix[0].length];
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    c[i][j] = matrix[i][j];
                 }
             }
 
@@ -77,95 +48,88 @@ public class ZeroMatrix {
     }
 
     /**
+     * First solution
+     */
+    static class FirstSolution {
+
+        public static void setZeros(int[][] matrix) {
+            boolean[] row = new boolean[matrix.length];
+            boolean[] column = new boolean[matrix[0].length];
+            // Store the row and column index with value 0
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    if (matrix[i][j] == 0) {
+                        row[i] = true;
+                        column[j] = true;
+                    }
+                }
+            }
+            // Nullify rows
+            for (int i = 0; i < row.length; i++) {
+                if (row[i]) {
+                    Common.nullifyRow(matrix, i);
+                }
+            }
+            // Nullify columns
+            for (int j = 0; j < column.length; j++) {
+                if (column[j]) {
+                    Common.nullifyColumn(matrix, j);
+                }
+            }
+        }
+    }
+
+    /**
      * Second solution
      */
     static class SecondSolution {
 
-        private static void nullifyRow(int[][] m, int r) {
-            for (int j = 0; j < m[0].length; j++) {
-                m[r][j] = 0;
-            }
-        }
-
-        private static void nullifyColumn(int[][] m, int c) {
-            for (int i = 0; i < m.length; i++) {
-                m[i][c] = 0;
-            }
-        }
-
-        public static void setZeros(int[][] m) {
+        public static void setZeros(int[][] matrix) {
             boolean rowHasZero = false;
             boolean colHasZero = false;
             // Check if first row has a zero
-            for (int j = 0; j < m[0].length; j++) {
-                if (m[0][j] == 0) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[0][j] == 0) {
                     rowHasZero = true;
                     break;
                 }
             }
             // Check if first column has a zero
-            for (int i = 0; i < m.length; i++) {
-                if (m[i][0] == 0) {
+            for (int i = 0; i < matrix.length; i++) {
+                if (matrix[i][0] == 0) {
                     colHasZero = true;
                     break;
                 }
             }
             // Check for zeros in the rest of the array
-            for (int i = 1; i < m.length; i++) {
-                for (int j = 1; j < m[0].length; j++) {
-                    if (m[i][j] == 0) {
-                        m[i][0] = 0;
-                        m[0][j] = 0;
+            for (int i = 1; i < matrix.length; i++) {
+                for (int j = 1; j < matrix[0].length; j++) {
+                    if (matrix[i][j] == 0) {
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
                     }
                 }
             }
             // Nullify rows based on values in first column
-            for (int i = 1; i < m.length; i++) {
-                if (m[i][0] == 0) {
-                    nullifyRow(m, i);
+            for (int i = 1; i < matrix.length; i++) {
+                if (matrix[i][0] == 0) {
+                    Common.nullifyRow(matrix, i);
                 }
             }
             // Nullify columns based on values in first row
-            for (int j = 1; j < m[0].length; j++) {
-                if (m[0][j] == 0) {
-                    nullifyColumn(m, j);
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[0][j] == 0) {
+                    Common.nullifyColumn(matrix, j);
                 }
             }
             // Nullify first row
             if (rowHasZero) {
-                nullifyRow(m, 0);
+                Common.nullifyRow(matrix, 0);
             }
             // Nullify first column
             if (colHasZero) {
-                nullifyColumn(m, 0);
+                Common.nullifyColumn(matrix, 0);
             }
-        }
-
-        private static boolean matricesAreEqual(int[][] m1, int[][] m2) {
-            if (m1.length != m2.length || m1[0].length != m2[0].length) {
-                return false;
-            }
-
-            for (int k = 0; k < m1.length; k++) {
-                for (int j = 0; j < m1[0].length; j++) {
-                    if (m1[k][j] != m2[k][j]) {
-                        return false;
-                    }
-                }
-            }
-
-            return true;
-        }
-
-        private static int[][] cloneMatrix(int[][] m) {
-            int[][] c = new int[m.length][m[0].length];
-            for (int i = 0; i < m.length; i++) {
-                for (int j = 0; j < m[0].length; j++) {
-                    c[i][j] = m[i][j];
-                }
-            }
-
-            return c;
         }
     }
 }
