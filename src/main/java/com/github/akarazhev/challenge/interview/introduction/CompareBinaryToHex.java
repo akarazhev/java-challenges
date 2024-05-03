938624cd3f5676189a796bd28ff01866fce92117
Andrey Karazhev <karazhev@gmail.com>
2024-05-03 13:43:10 +0300

    Added the CompareBinaryToHex sample.

---
+++ src/main/java/com/github/akarazhev/challenge/interview/introduction/CompareBinaryToHex.java
@@ -0,0 +1,45 @@
+package com.github.akarazhev.challenge.interview.introduction;
+
+public class CompareBinaryToHex {
+
+    private static int digitToValue(char c) {
+        if (c >= '0' && c <= '9') {
+            return c - '0';
+        } else if (c >= 'A' && c <= 'F') {
+            return 10 + c - 'A';
+        } else if (c >= 'a' && c <= 'f') {
+            return 10 + c - 'a';
+        }
+
+        return -1;
+    }
+
+    private static int convertFromBase(String number, int base) {
+        if (base < 2 || (base > 10 && base != 16)) {
+            return -1;
+        }
+
+        int exp, value = 0;
+        for (int i = number.length() - 1; i >= 0; i--) {
+            int digit = digitToValue(number.charAt(i));
+            if (digit < 0 || digit >= base) {
+                return -1;
+            }
+
+            exp = number.length() - 1 - i;
+            value += (int) (digit * Math.pow(base, exp));
+        }
+
+        return value;
+    }
+
+    public static boolean compareBinToHex(String binary, String hex) {
+        int n1 = convertFromBase(binary, 2);
+        int n2 = convertFromBase(hex, 16);
+        if (n1 < 0 || n2 < 0) {
+            return false;
+        }
+
+        return n1 == n2;
+    }
+}
---
+++ src/test/java/com/github/akarazhev/challenge/interview/introduction/CompareBinaryToHexTest.java
@@ -0,0 +1,16 @@
+package com.github.akarazhev.challenge.interview.introduction;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Compare Binary To Hex Test")
+public class CompareBinaryToHexTest {
+
+    @Test
+    @DisplayName("Compare Bin To Hex")
+    public void compareBinToHex() {
+        assertTrue(CompareBinaryToHex.compareBinToHex("111001011", "1CB"));
+    }
+}
