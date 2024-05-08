927780d347a09feb72ff340b36bfb4bb052240d6
Andrey Karazhev <karazhev@gmail.com>
2024-05-08 23:35:49 +0300

    Added the question05 of the big O.

--- src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Question05.java
+++ src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Question05.java
@@ -1,21 +1,23 @@
 package com.github.akarazhev.challenge.interview.introduction.bigo;
 
-public class Question05 {
+public final class Question05 {
 
-    public static int sqrt(int n) {
-        return sqrt_helper(n, 1, n);
+    public static int sqrt(final int n) {
+        return sqrtHelper(n, 1, n);
     }
 
-    public static int sqrt_helper(int n, int min, int max) {
-        if (max < min) return -1; // no square root
+    private static int sqrtHelper(final int n, final int min, final int max) {
+        if (max < min) {
+            return -1; // no square root
+        }
 
         int guess = (min + max) / 2;
         if (guess * guess == n) { // found it!
             return guess;
         } else if (guess * guess < n) { // too low
-            return sqrt_helper(n, guess + 1, max); // try higher
+            return sqrtHelper(n, guess + 1, max); // try higher
         } else { // too high
-            return sqrt_helper(n, min, guess - 1); // try lower
+            return sqrtHelper(n, min, guess - 1); // try lower
         }
     }
 
---
+++ src/test/java/com/github/akarazhev/challenge/interview/introduction/bigo/Question05Test.java
@@ -0,0 +1,16 @@
+package com.github.akarazhev.challenge.interview.introduction.bigo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Question 05 Test")
+public final class Question05Test {
+
+    @Test
+    @DisplayName("Test Sqrt")
+    public void testSqrt() {
+        assertEquals(3, Question05.sqrt(26));
+    }
+}
