97ee42482b8f61cbead71ceceec826cb1d6b206b
Andrey Karazhev <karazhev@gmail.com>
2024-05-08 23:47:04 +0300

    Added the question11 of the big O.

--- src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Question11.java
+++ src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Question11.java
@@ -1,14 +1,14 @@
 package com.github.akarazhev.challenge.interview.introduction.bigo;
 
-public class Question11 {
+public final class Question11 {
 
     public static int numChars = 26;
 
-    public static void printSortedStrings(int remaining) {
+    public static void printSortedStrings(final int remaining) {
         printSortedStrings(remaining, "");
     }
 
-    public static void printSortedStrings(int remaining, String prefix) {
+    private static void printSortedStrings(final int remaining, final String prefix) {
         if (remaining == 0) {
             System.out.println(prefix);
         } else {
@@ -19,7 +19,7 @@ public class Question11 {
         }
     }
 
-    public static boolean isInOrder(String s) {
+    private static boolean isInOrder(final String s) {
         for (int i = 1; i < s.length(); i++) {
             int prev = ithLetter(s.charAt(i - 1));
             int curr = ithLetter(s.charAt(i));
@@ -27,14 +27,11 @@ public class Question11 {
                 return false;
             }
         }
+
         return true;
     }
 
-    public static char ithLetter(int i) {
+    private static char ithLetter(final int i) {
         return (char) (((int) 'a') + i);
-    }
-
-    public static void main(String[] args) {
-        printSortedStrings(3);
     }
 }
---
+++ src/test/java/com/github/akarazhev/challenge/interview/introduction/bigo/Question11Test.java
@@ -0,0 +1,14 @@
+package com.github.akarazhev.challenge.interview.introduction.bigo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Question 11 Test")
+public final class Question11Test {
+
+    @Test
+    @DisplayName("Test Print Sorted Strings")
+    public void testPrintSortedStrings() {
+        Question11.printSortedStrings(3);
+    }
+}
