ba5dfe0f68548a185d3bae646f2d4b5def7f3554
Andrey Karazhev <karazhev@gmail.com>
2024-05-08 23:14:15 +0300

    Updated the example19 of the big O.

---
+++ src/test/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example19Test.java
@@ -0,0 +1,14 @@
+package com.github.akarazhev.challenge.interview.introduction.bigo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Example 19 Test")
+public final class Example19Test {
+
+    @Test
+    @DisplayName("Test All Fib")
+    public void testAllFib() {
+        Example19.allFib(6);
+    }
+}
