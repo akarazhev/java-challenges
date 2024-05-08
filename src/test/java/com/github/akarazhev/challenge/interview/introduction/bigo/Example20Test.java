82eb7f171c4a8cb21350fdf5076af135f88ec89a
Andrey Karazhev <karazhev@gmail.com>
2024-05-08 23:16:03 +0300

    Updated the example20 of the big O.

---
+++ src/test/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example20Test.java
@@ -0,0 +1,14 @@
+package com.github.akarazhev.challenge.interview.introduction.bigo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Example 20 Test")
+public final class Example20Test {
+
+    @Test
+    @DisplayName("Test Powers Of 2")
+    public void testPowersOf2() {
+        Example20.powersOf2(100);
+    }
+}
