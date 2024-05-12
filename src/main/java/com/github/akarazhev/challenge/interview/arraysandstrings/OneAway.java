944120319f2848cd760ef2de7bce0e323795c026
Andrey Karazhev <karazhev@gmail.com>
2024-05-12 19:26:56 +0300

    Updated variables in the OneAway.

--- src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/OneAway.java
+++ src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/OneAway.java
@@ -17,10 +17,10 @@ public class OneAway {
      */
     static class FirstSolution {
 
-        private static boolean oneEditReplace(String firstString, String secondString) {
+        private static boolean oneEditReplace(String s1, String s2) {
             boolean foundDifference = false;
-            for (int i = 0; i < firstString.length(); i++) {
-                if (firstString.charAt(i) != secondString.charAt(i)) {
+            for (int i = 0; i < s1.length(); i++) {
+                if (s1.charAt(i) != s2.charAt(i)) {
                     if (foundDifference) {
                         return false;
                     }
@@ -33,21 +33,21 @@ public class OneAway {
         }
 
         /**
-         * Check if you can insert a character into firstString to make secondString.
+         * Check if you can insert a character into s1 to make s2.
          */
-        private static boolean oneEditInsert(String firstString, String secondString) {
-            int firstIndex = 0;
-            int secondIndex = 0;
-            while (secondIndex < secondString.length() && firstIndex < firstString.length()) {
-                if (firstString.charAt(firstIndex) != secondString.charAt(secondIndex)) {
-                    if (firstIndex != secondIndex) {
+        private static boolean oneEditInsert(String s1, String s2) {
+            int index1 = 0;
+            int index2 = 0;
+            while (index2 < s2.length() && index1 < s1.length()) {
+                if (s1.charAt(index1) != s2.charAt(index2)) {
+                    if (index1 != index2) {
                         return false;
                     }
 
-                    secondIndex++;
+                    index2++;
                 } else {
-                    firstIndex++;
-                    secondIndex++;
+                    index1++;
+                    index2++;
                 }
             }
 
@@ -57,13 +57,13 @@ public class OneAway {
         /**
          * This function takes two strings and checks if they are one edit (or zero edits) away.
          */
-        public static boolean oneEditAway(String firstString, String secondString) {
-            if (firstString.length() == secondString.length()) {
-                return oneEditReplace(firstString, secondString);
-            } else if (firstString.length() + 1 == secondString.length()) {
-                return oneEditInsert(firstString, secondString);
-            } else if (firstString.length() - 1 == secondString.length()) {
-                return oneEditInsert(secondString, firstString);
+        public static boolean oneEditAway(String first, String second) {
+            if (first.length() == second.length()) {
+                return oneEditReplace(first, second);
+            } else if (first.length() + 1 == second.length()) {
+                return oneEditInsert(first, second);
+            } else if (first.length() - 1 == second.length()) {
+                return oneEditInsert(second, first);
             }
 
             return false;
