f15a138f8e91e94772f35c63013a0998f7aef687
Andrey Karazhev <karazhev@gmail.com>
2024-05-12 19:49:20 +0300

    Updated variables in the ZeroMatrix.

--- src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/ZeroMatrix.java
+++ src/main/java/com/github/akarazhev/challenge/interview/arraysandstrings/ZeroMatrix.java
@@ -3,30 +3,30 @@ package com.github.akarazhev.challenge.interview.arraysandstrings;
 /**
  * Statement: Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0.
  */
-public class ZeroMatrix {
+public final class ZeroMatrix {
 
-    private static class Common {
+    private final static class Common {
 
-        private static void nullifyRow(int[][] matrix, int row) {
+        private static void nullifyRow(final int[][] matrix, final int row) {
             for (int j = 0; j < matrix[0].length; j++) {
                 matrix[row][j] = 0;
             }
         }
 
-        private static void nullifyColumn(int[][] matrix, int col) {
+        private static void nullifyColumn(final int[][] matrix, final int col) {
             for (int i = 0; i < matrix.length; i++) {
                 matrix[i][col] = 0;
             }
         }
 
-        private static boolean matricesAreEqual(int[][] firstMatrix, int[][] secondMatrix) {
-            if (firstMatrix.length != secondMatrix.length || firstMatrix[0].length != secondMatrix[0].length) {
+        private static boolean matricesAreEqual(final int[][] m1, final int[][] m2) {
+            if (m1.length != m2.length || m1[0].length != m2[0].length) {
                 return false;
             }
 
-            for (int k = 0; k < firstMatrix.length; k++) {
-                for (int j = 0; j < firstMatrix[0].length; j++) {
-                    if (firstMatrix[k][j] != secondMatrix[k][j]) {
+            for (int k = 0; k < m1.length; k++) {
+                for (int j = 0; j < m1[0].length; j++) {
+                    if (m1[k][j] != m2[k][j]) {
                         return false;
                     }
                 }
@@ -35,7 +35,7 @@ public class ZeroMatrix {
             return true;
         }
 
-        private static int[][] cloneMatrix(int[][] matrix) {
+        private static int[][] cloneMatrix(final int[][] matrix) {
             int[][] c = new int[matrix.length][matrix[0].length];
             for (int i = 0; i < matrix.length; i++) {
                 for (int j = 0; j < matrix[0].length; j++) {
@@ -50,9 +50,9 @@ public class ZeroMatrix {
     /**
      * First solution
      */
-    static class FirstSolution {
+    static final class FirstSolution {
 
-        public static void setZeros(int[][] matrix) {
+        public static void setZeros(final int[][] matrix) {
             boolean[] row = new boolean[matrix.length];
             boolean[] column = new boolean[matrix[0].length];
             // Store the row and column index with value 0
@@ -82,9 +82,9 @@ public class ZeroMatrix {
     /**
      * Second solution
      */
-    static class SecondSolution {
+    static final class SecondSolution {
 
-        public static void setZeros(int[][] matrix) {
+        public static void setZeros(final int[][] matrix) {
             boolean rowHasZero = false;
             boolean colHasZero = false;
             // Check if first row has a zero
