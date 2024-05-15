2baae787ddf1ab6d03efd6c7268f19c14e4568e1
Andrey Karazhev <karazhev@gmail.com>
2024-05-15 09:34:10 +0300

    Added tests for the ZeroMatrix.

--- src/test/java/com/github/akarazhev/challenge/interview/arraysandstrings/ZeroMatrixTest.java
+++ src/test/java/com/github/akarazhev/challenge/interview/arraysandstrings/ZeroMatrixTest.java
@@ -1,10 +1,32 @@
 package com.github.akarazhev.challenge.interview.arraysandstrings;
 
+import com.github.akarazhev.challenge.interview.util.AssortedMethods;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 /**
  * Provides tests for the <code>Zero Matrix</code> solution.
  */
 @DisplayName("Zero Matrix Test")
 public class ZeroMatrixTest {
+
+    @Test
+    @DisplayName("Test First Solution")
+    void testFirstSolution() {
+        int[][] matrix = AssortedMethods.randomMatrix(10, 15, -10, 10);
+        AssortedMethods.printMatrix(matrix);
+        ZeroMatrix.FirstSolution.setZeros(matrix);
+        System.out.println();
+        AssortedMethods.printMatrix(matrix);
+    }
+
+    @Test
+    @DisplayName("Test Second Solution")
+    void testSecondSolution() {
+        int[][] matrix = AssortedMethods.randomMatrix(10, 15, -10, 10);
+        AssortedMethods.printMatrix(matrix);
+        ZeroMatrix.SecondSolution.setZeros(matrix);
+        System.out.println();
+        AssortedMethods.printMatrix(matrix);
+    }
 }
