f6cda704924a1c112f2393b6247824499ad1cb50
Andrey Karazhev <karazhev@gmail.com>
2024-05-20 10:16:36 +0300

    Updated the example 6 of the Big O chapter.

--- src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example06.java
+++ src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example06.java
@@ -1,8 +1,6 @@
 package com.github.akarazhev.challenge.interview.introduction.bigo;
 
 /**
- * What is the runtime of the below code?
- * <p>
  * This will take O(N) time. The fact that we iterate through the array twice doesn't matter.
  */
 public final class Example06 {
@@ -10,12 +8,12 @@ public final class Example06 {
     public static void foo(final int[] array) {
         int sum = 0;
         int product = 1;
-        for (int i = 0; i < array.length; i++) {
-            sum += array[i];
+        for (int j : array) {
+            sum += j;
         }
 
-        for (int i = 0; i < array.length; i++) {
-            product *= array[i];
+        for (int j : array) {
+            product *= j;
         }
 
         System.out.println(sum + ", " + product);
