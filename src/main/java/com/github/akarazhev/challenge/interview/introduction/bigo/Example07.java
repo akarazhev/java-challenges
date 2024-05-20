258282abeb689979113fe6c0586c2d0931a8d63e
Andrey Karazhev <karazhev@gmail.com>
2024-05-20 10:45:51 +0300

    Updated the example 7 of the Big O chapter.

--- src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example07.java
+++ src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example07.java
@@ -2,10 +2,13 @@ package com.github.akarazhev.challenge.interview.introduction.bigo;
 
 public final class Example07 {
 
+    /**
+     * The inner for loop has O(N) iterations, and it is called N times. Therefore, the runtime is O(N^2).
+     */
     public static void printPairs(final int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                System.out.println(array[i] + "," + array[j]);
+        for (int k : array) {
+            for (int i : array) {
+                System.out.println(k + "," + i);
             }
         }
     }
