d4d69b5abc81c4050d36db07abe18261e10a7ed6
Andrey Karazhev <karazhev@gmail.com>
2024-05-20 10:15:17 +0300

    Updated the example 5 of the Big O chapter.

--- src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example05.java
+++ src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example05.java
@@ -2,6 +2,11 @@ package com.github.akarazhev.challenge.interview.introduction.bigo;
 
 public final class Example05 {
 
+    /**
+     * When you have a recursive function that makes multiple calls, the runtime will often (but not always) look
+     * like O(branches^depth), where branches is the number of times each recursive call branches. In this case,
+     * this gives us O(2^N).
+     */
     public static int f(final int n) {
         if (n <= 0) {
             return 1;
