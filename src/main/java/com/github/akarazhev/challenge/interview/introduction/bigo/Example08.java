7bfaf970312d2123d86536df6c5bcfc9be6fe353
Andrey Karazhev <karazhev@gmail.com>
2024-05-20 11:04:46 +0300

    Updated the example 8 of the Big O chapter.

--- src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example08.java
+++ src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example08.java
@@ -2,6 +2,10 @@ package com.github.akarazhev.challenge.interview.introduction.bigo;
 
 public final class Example08 {
 
+    /**
+     * There are N^2 total pairs. Roughly half of those will have i < j and the remaining half will have i > j.
+     * This code goes through roughly N^2/2 pairs, so it does O(N^2) work.
+     */
     public static void printUnorderedPairs(final int[] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = i; j < array.length; j++) {
