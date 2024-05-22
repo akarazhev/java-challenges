825bff72aed71d97c8b0e8adcf586610ab08d603
Andrey Karazhev <karazhev@gmail.com>
2024-05-22 09:29:05 +0300

    Updated the example 14 of the Big O chapter.

--- src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example14.java
+++ src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example14.java
@@ -2,6 +2,9 @@ package com.github.akarazhev.challenge.interview.introduction.bigo;
 
 public final class Example14 {
 
+    /**
+     * This runs in O(sqrt(n)) time.
+     */
     public static boolean isPrime(final int n) {
         for (int x = 2; x * x <= n; x++) {
             if (n % x == 0) {
