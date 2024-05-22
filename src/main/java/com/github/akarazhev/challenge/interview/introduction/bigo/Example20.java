bbea66274520e92b2d19c1a466fb80f21338e043
Andrey Karazhev <karazhev@gmail.com>
2024-05-22 09:42:07 +0300

    Updated the example 20 of the Big O chapter.

--- src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example20.java
+++ src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example20.java
@@ -2,6 +2,9 @@ package com.github.akarazhev.challenge.interview.introduction.bigo;
 
 public final class Example20 {
 
+    /**
+     * There are log N powers of 2 between 1 and n. Therefore, the runtime is 0(log n).
+     */
     public static int powersOf2(final int n) {
         if (n == 1) {
             System.out.println(1);
