5e29d06fc60dc5b8590433cbe20082a0a341529c
Andrey Karazhev <karazhev@gmail.com>
2024-05-22 09:26:40 +0300

    Updated the example 11 of the Big O chapter.

--- src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example11.java
+++ src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example11.java
@@ -2,6 +2,10 @@ package com.github.akarazhev.challenge.interview.introduction.bigo;
 
 public final class Example11 {
 
+    /**
+     * This algorithm runs in O(N) time. The fact that it only goes through half of the array (in terms of iterations)
+     * does not impact the big O time.
+     */
     public static void reverse(final int[] array) {
         for (int i = 0; i < array.length / 2; i++) {
             int other = array.length - i - 1;
