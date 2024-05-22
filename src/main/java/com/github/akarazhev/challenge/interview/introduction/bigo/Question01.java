ee5f7bf5d5a4a3e935b2bfac39932f4917ff360a
Andrey Karazhev <karazhev@gmail.com>
2024-05-22 12:30:21 +0300

    Updated the Question01 of the Big O chapter.

--- src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Question01.java
+++ src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Question01.java
@@ -2,6 +2,10 @@ package com.github.akarazhev.challenge.interview.introduction.bigo;
 
 public final class Question01 {
 
+    /**
+     * The following code computes the product of a and b. What is its runtime?
+     * O(b). The for loop just iterates through b.
+     */
     public static int product(final int a, final int b) {
         int sum = 0;
         for (int i = 0; i < b; i++) {
