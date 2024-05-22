6477747c0b601b4bbda45e1773fe4f179820a703
Andrey Karazhev <karazhev@gmail.com>
2024-05-22 09:30:10 +0300

    Updated the example 15 of the Big O chapter.

--- src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example15.java
+++ src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example15.java
@@ -3,7 +3,7 @@ package com.github.akarazhev.challenge.interview.introduction.bigo;
 public final class Example15 {
 
     /**
-     * It will take O(n) time
+     * It will take O(n) time.
      */
     public static int factorial(final int n) {
         if (n < 0) {
