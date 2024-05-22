a71872d80c14f4a6bef7709c318d78039d0570a9
Andrey Karazhev <karazhev@gmail.com>
2024-05-22 08:50:58 +0300

    Updated the example 9 of the Big O chapter.

--- src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example09.java
+++ src/main/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example09.java
@@ -2,11 +2,16 @@ package com.github.akarazhev.challenge.interview.introduction.bigo;
 
 public final class Example09 {
 
+    /**
+     * The if-statement within j's for loop is 0(1) time since it's just a sequence of constant-time statements.
+     * For each element of arrayA, the inner for loop goes through b iterations, where b = arrayB.length.
+     * if a = arrayA.length, then the runtime is O(ab).
+     */
     public static void printUnorderedPairs(final int[] arrayA, final int[] arrayB) {
-        for (int i = 0; i < arrayA.length; i++) {
-            for (int j = 0; j < arrayB.length; j++) {
-                if (arrayA[i] < arrayB[j]) {
-                    System.out.println(arrayA[i] + "," + arrayB[j]);
+        for (int k : arrayA) {
+            for (int i : arrayB) {
+                if (k < i) {
+                    System.out.println(k + "," + i);
                 }
             }
         }
