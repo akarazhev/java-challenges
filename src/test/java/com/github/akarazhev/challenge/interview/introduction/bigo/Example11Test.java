dcb336bd59eb24b9b425938447f36f332168416d
andrey karazhev <karazhev@gmail.com>
2024-06-03 11:20:15 +0300

    Updated unit tests.

--- src/test/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example11Test.java
+++ src/test/java/com/github/akarazhev/challenge/interview/introduction/bigo/Example11Test.java
@@ -3,6 +3,8 @@ package com.github.akarazhev.challenge.interview.introduction.bigo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @DisplayName("Example 11 Test")
 public final class Example11Test {
 
@@ -11,8 +13,11 @@ public final class Example11Test {
     public void testReverse() {
         int[] array = {0, 1, 2, 3, 4, 5};
         Example11.reverse(array);
-        for (int x : array) {
-            System.out.println(x);
-        }
+        assertEquals(5, array[0]);
+        assertEquals(4, array[1]);
+        assertEquals(3, array[2]);
+        assertEquals(2, array[3]);
+        assertEquals(1, array[4]);
+        assertEquals(0, array[5]);
     }
 }
