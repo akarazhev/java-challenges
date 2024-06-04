17bbfcf1c2068d9d2a08ad391855f778c6cd123b
andrey karazhev <karazhev@gmail.com>
2024-06-04 10:10:25 +0300

    Updated the Intersection solution with tests.

--- src/main/java/com/github/akarazhev/challenge/interview/linkedlists/Intersection.java
+++ src/main/java/com/github/akarazhev/challenge/interview/linkedlists/Intersection.java
@@ -2,19 +2,22 @@ package com.github.akarazhev.challenge.interview.linkedlists;
 
 import com.github.akarazhev.challenge.interview.util.LinkedListNode;
 
+/**
+ * Statement:
+ */
 public final class Intersection {
 
-    public static class Result {
-        public LinkedListNode tail;
-        public int size;
+    public final static class Result {
+        public final LinkedListNode tail;
+        public final int size;
 
-        public Result(LinkedListNode tail, int size) {
+        public Result(final LinkedListNode tail, final int size) {
             this.tail = tail;
             this.size = size;
         }
     }
 
-    public static Result getTailAndSize(LinkedListNode list) {
+    public static Result getTailAndSize(final LinkedListNode list) {
         if (list == null) {
             return null;
         }
@@ -29,7 +32,7 @@ public final class Intersection {
         return new Result(current, size);
     }
 
-    public static LinkedListNode getKthNode(LinkedListNode head, int k) {
+    public static LinkedListNode getKthNode(final LinkedListNode head, int k) {
         LinkedListNode current = head;
         while (k > 0 && current != null) {
             current = current.next;
@@ -39,31 +42,27 @@ public final class Intersection {
         return current;
     }
 
-    public static LinkedListNode findIntersection(LinkedListNode list1, LinkedListNode list2) {
-        if (list1 == null || list2 == null) return null;
-
+    public static LinkedListNode findIntersection(final LinkedListNode list1, final LinkedListNode list2) {
+        if (list1 == null || list2 == null) {
+            return null;
+        }
         /* Get tail and sizes. */
         Result result1 = getTailAndSize(list1);
         Result result2 = getTailAndSize(list2);
-
         /* If different tail nodes, then there's no intersection. */
         if (result1.tail != result2.tail) {
             return null;
         }
-
         /* Set pointers to the start of each linked list. */
         LinkedListNode shorter = result1.size < result2.size ? list1 : list2;
         LinkedListNode longer = result1.size < result2.size ? list2 : list1;
-
         /* Advance the pointer for the longer linked list by the difference in lengths. */
         longer = getKthNode(longer, Math.abs(result1.size - result2.size));
-
         /* Move both pointers until you have a collision. */
         while (shorter != longer) {
             shorter = shorter.next;
             longer = longer.next;
         }
-
         /* Return either one. */
         return longer;
     }
--- src/test/java/com/github/akarazhev/challenge/interview/linkedlists/IntersectionTest.java
+++ src/test/java/com/github/akarazhev/challenge/interview/linkedlists/IntersectionTest.java
@@ -1,4 +1,28 @@
 package com.github.akarazhev.challenge.interview.linkedlists;
 
-public class IntersectionTest {
+import com.github.akarazhev.challenge.interview.util.AssortedMethods;
+import com.github.akarazhev.challenge.interview.util.LinkedListNode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Provides tests for the statement:
+ */
+@DisplayName("Intersection Test")
+public final class IntersectionTest {
+
+    @Test
+    @DisplayName("Test Find Intersection")
+    void testFindIntersection() {
+        /* Create linked list */
+        LinkedListNode list1 = AssortedMethods.createLinkedListFromArray(new int[]{-1, -2, 0, 1, 2, 3, 4, 5, 6, 7, 8});
+        LinkedListNode list2 = AssortedMethods.createLinkedListFromArray(new int[]{12, 14, 15});
+        list2.next.next = list1.next.next.next.next;
+        assertEquals("-1->-2->0->1->2->3->4->5->6->7->8", list1.printForward());
+        assertEquals("12->14->2->3->4->5->6->7->8", list2.printForward());
+        LinkedListNode intersection = Intersection.findIntersection(list1, list2);
+        assertEquals("2->3->4->5->6->7->8", intersection.printForward());
+    }
 }
